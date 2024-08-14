8032a07a09a8741521cda7864a4931a39a84fe01
Abstract Programmer <shireloskar8@gmail.com>
2024-08-14 18:31:46 +0300

    <meaningful message that describes the commit>

--- Shirel.java
+++ Shirel.java
@@ -1,6 +1,6 @@
 public class Shirel{
 public static void main(String[] args)
-{for (int i=1;i<=10;i++){System.out.println("Shirel Oskar" + i);
+{for (int i=1;i<=10;i++){System.out.println(" no Shirel Oskar" + i);
 }
 }
 }
